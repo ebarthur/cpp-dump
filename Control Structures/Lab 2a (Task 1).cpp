a7e5072270ce6bb44387daebae04d9cbf8c802cc
Aartt! <151869774+ebarthur@users.noreply.github.com>
2023-12-11 17:47:06 +0000

    Update Lab 2a (Task 1).cpp
    Added comments to enhance code readability

--- Control Structures/Lab 2a (Task 1).cpp
+++ Control Structures/Lab 2a (Task 1).cpp
@@ -4,65 +4,69 @@ using namespace std;
 
 int main()
 {
+    // Declare a variable to store the user-inputted month
+    int month;
 
-int month;
+    // Prompt the user to enter a month number
+    cout << "Enter month number: " << endl;
 
-cout << "Enter month number: " << endl;
+    // Read the user's input into the 'month' variable
+    cin >> month;
 
-cin >> month;
+    // Output the corresponding month name based on the user's input
+    cout << "The month is ";
 
-cout << "The month is ";
-
-if (month == 1)
+    // Check each possible month number and print the corresponding month name
+    if (month == 1)
     {
-    cout << "January" << endl;
+        cout << "January" << endl;
+    }
+    else if (month == 2)
+    {
+        cout << "February" << endl;
+    }
+    else if (month == 3)
+    {
+        cout << "March" << endl;
+    }
+    else if (month == 4)
+    {
+        cout << "April" << endl;
+    }
+    else if (month == 5)
+    {
+        cout << "May" << endl;
+    }
+    else if (month == 6)
+    {
+        cout << "June" << endl;
+    }
+    else if (month == 7)
+    {
+        cout << "July" << endl;
+    }
+    else if (month == 8)
+    {
+        cout << "August" << endl;
+    }
+    else if (month == 9)
+    {
+        cout << "September" << endl;
+    }
+    else if (month == 10)
+    {
+        cout << "October" << endl;
+    }
+    else if (month == 11)
+    {
+        cout << "November" << endl;
+    }
+    else if (month == 12)
+    {
+        cout << "December" << endl;
+    }
+    else {
+        // If the entered month number is not in the range 1-12, output "Invalid"
+        cout << "Invalid";
+    }
 }
-else if (month == 2)
-{
-    cout << "February" << endl;
-}
-else if (month == 3)
-{
-    cout << "March" << endl;
-}
-else if (month == 4)
-{
-    cout << "April" << endl;
-}
-else if (month == 5)
-{
-    cout << "May" << endl;
-}
-else if (month == 6)
-{
-    cout << "June" << endl;
-}
-else if (month == 7)
-{
-    cout << "July" << endl;
-}
-else if (month == 8)
-{
-    cout << "August" << endl;
-}
-else if (month == 9)
-{
-    cout << "September" << endl;
-}
-else if (month == 10)
-{
-    cout << "October" << endl;
-}
-else if (month == 11)
-{
-    cout << "November" << endl;
-}
-else if (month == 12)
-{
-    cout << "December" << endl;
-}
-else {
-    cout << "Invalid";
-}
-}
-
