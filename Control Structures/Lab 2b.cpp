f8d3db54c4e6cb77c8ab53c0b6a89f4f546c3588
Ebenezer Arthur <151869774+ebarthur@users.noreply.github.com>
2023-12-11 17:02:21 +0000

    Update Challenges
    I added some tasks on control structures in cpp programming.

---
+++ Control Structures/Lab 2b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int j;
+    for (int i = 0; i < 3; i++) {
+        cout << "XOXOXOXOX" << endl;
+        for (int j = 0; j < 2; j++) {
+            cout << "OXO" << endl;
+        }
+
+    }
+}
