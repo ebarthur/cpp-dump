6ea97b71a523ab95dc41a0dba23d1db6e95f5a10
Aartt! <151869774+ebarthur@users.noreply.github.com>
2023-12-11 17:52:29 +0000

    Update main.cpp
    Added comments for code readability

--- arrays/solution/challenge/main.cpp
+++ arrays/solution/challenge/main.cpp
@@ -4,35 +4,42 @@ using namespace std;
 
 int main()
 {
+    // Define a 2D array to represent a chessboard
     string chessboard[8][8];
-    int row = sizeof(chessboard)/ sizeof(chessboard[0]);
-    int col = sizeof(chessboard[0])/sizeof(string);
-//solution start
-string board[8][8] =  {{"O", "X", "O", "X", "O", "X", "O", "X"},
-                         {"X", "O", "X", "O", "X", "O", "X", "O"},
-                         {"O", "X", "O", "X", "O", "X", "O", "X"},
-                         {"X", "O", "X", "O", "X", "O", "X", "O"},
-                         {"O", "X", "O", "X", "O", "X", "O", "X"},
-                         {"X", "O", "X", "O", "X", "O", "X", "O"},
-                         {"O", "X", "O", "X", "O", "X", "O", "X"},
-                         {"X", "O", "X", "O", "X", "O", "X", "O"} };
 
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < col ; j++){
-            chessboard[i][j]= board[i][j];
+    // Determine the number of rows and columns in the chessboard
+    int row = sizeof(chessboard) / sizeof(chessboard[0]);
+    int col = sizeof(chessboard[0]) / sizeof(string);
 
+    // Initialize the chessboard with an alternating pattern of "O" and "X"
+    string board[8][8] = {{"O", "X", "O", "X", "O", "X", "O", "X"},
+                          {"X", "O", "X", "O", "X", "O", "X", "O"},
+                          {"O", "X", "O", "X", "O", "X", "O", "X"},
+                          {"X", "O", "X", "O", "X", "O", "X", "O"},
+                          {"O", "X", "O", "X", "O", "X", "O", "X"},
+                          {"X", "O", "X", "O", "X", "O", "X", "O"},
+                          {"O", "X", "O", "X", "O", "X", "O", "X"},
+                          {"X", "O", "X", "O", "X", "O", "X", "O"} };
+
+    // Copy the pattern to the chessboard
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            chessboard[i][j] = board[i][j];
         }
     }
-//solution end
-    for (int i = 0;i < row; i++ ){
-            for (int j = 0;j < col; j++ ){
-                    if(j == col - 1){
-                        cout<<chessboard[i][j]<<endl;
-                    }
-                    else{
-                        cout<<chessboard[i][j];
-                    }
+
+    // Display the chessboard
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (j == col - 1) {
+                // Print a new line at the end of each row
+                cout << chessboard[i][j] << endl;
+            } else {
+                // Print the chessboard cell
+                cout << chessboard[i][j];
             }
+        }
     }
+
     return 0;
 }
